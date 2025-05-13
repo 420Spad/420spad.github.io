a4ea48558dd2263600431a94872f6d5fbdc7ebf5
420Spad <49189919+420Spad@users.noreply.github.com>
2025-05-13 19:39:09 +0200

    test

---
+++ app-files/data.js
@@ -0,0 +1,56 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.6678815771880728,
+        "pitch": -0.05752018582004581,
+        "fov": 1.3694074884773513
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0828147525393597,
+          "pitch": 1.1439868749752584,
+          "rotation": 6.283185307179586,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4848428447448718,
+          "pitch": 0.3419509645062533,
+          "title": "And this is the title<div>line2</div>",
+          "text": "Info hot spot here"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
